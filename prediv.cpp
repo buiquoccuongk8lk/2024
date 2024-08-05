b3e6dc8db8c554f198a7f63cd48d770cde32381c
buiquoccuongk8lk <167599592+buiquoccuongk8lk@users.noreply.github.com>
2024-08-05 10:39:37 +0700

    THMN 2024

---
+++ prediv.cpp
@@ -0,0 +1,88 @@
+/*[продолжатьипродолжать]*/
+/* ---------------------- *
+ *     Code by watbor     *
+ *                        * 
+--------------------------*/
+
+#define         tct template<class MITSURII,class CHANN>
+
+#include <bits/stdc++.h>
+using namespace std ; 
+
+#define ll long long 
+#define fi first
+#define se second
+#define pb push_back
+#define ii pair<int,int>
+#define vii vector<ii>
+#define vi vector<int>
+#define endl '\n' 
+#define sz(a) (int)a.size()
+#define all(a) a.begin(),a.end()
+#define FOR(i,a,b) for(int i(a) ; i <= b ; i++)
+#define FORD(i,a,b) for(int i(a) ; i >= b ; i--)
+#define FORN(i,a,b) for(int i(a) ; i < b ; i++)
+#define uni(a) sort(all(a)) , a.resize(unique(a.begin(),a.end())-a.begin()) 
+
+const int N = 1e6 + 5 ;
+const int LO = 17 ;
+const int base = 311 ;
+const int M = 1e3 + 5 ; 
+const int oo = 2e9 ; 
+const int mod = 1e9 + 7 ;
+const long long inf = 1e18 ;
+
+const int dx[] = {-1,1,0,0} ;
+const int dy[] = {0,0,-1,1} ; 
+
+tct bool minimize(MITSURII &x,const CHANN &y) {if(x > y) {x = y ; return true ; }else return false ;}
+tct bool maximize(MITSURII &x,const CHANN &y) {if(x < y) {x = y ; return true ; }else return false ;}
+
+int n , q ; 
+int a[N] , p[N] , cnt[N] , dem[N] ;
+vi zip ; 
+ll pre[N] ; 
+
+void init() {
+	cin >> n >> q ; 
+	FOR(i,1,n) cin >> a[i] ; 
+	FOR(i,1,q) cin >> p[i] ; 
+}
+
+void Sieve() {
+	FOR(i,1,1e6) {
+		for(int j(i) ; j <= 1e6 ; j+=i) {
+			dem[i]+= cnt[j] ; 
+		}
+	}
+}
+
+void solve() {
+	FOR(i,1,n)cnt[a[i]]++ ; 
+	Sieve() ; 
+	FOR(i,1,1e6) if(dem[i]) zip.pb(i) ; 
+	FOR(i,0,sz(zip)-1) pre[i] = pre[i-1] + dem[zip[i]] ; 
+	FOR(i,1,q) {
+		int l = 0 , r = sz(zip) - 1 , ans = -1 ; 
+		while(l <= r) {
+			int mid = (l+r)>>1 ; 
+			if(pre[mid] >= p[i]) {
+				ans = mid ;
+				r = mid - 1 ; 
+			} else l = mid + 1 ; 
+		}
+		cout << zip[ans] << ' ' ;
+	}
+}
+
+signed main() {
+    #define TASK ""
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr);cout.tie(nullptr);
+    if( fopen(TASK".inp","r") ) {
+        freopen(TASK".inp","r",stdin) ; freopen(TASK".out","w",stdout);
+    }
+    init() ; solve() ; 
+    cerr << endl << "watborhihi : " << clock() << "ms" << endl;
+    return 0 ;
+}
